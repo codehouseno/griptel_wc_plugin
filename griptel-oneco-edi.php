c7ecea79d5ad06cce56b911e268fbeaaaa3a23ca
MrElgheim <71493700+Elgheim@users.noreply.github.com>
2023-11-10 15:06:54 +0100

    fix: removed used code

--- griptel-oneco-edi.php
+++ griptel-oneco-edi.php
@@ -265,25 +265,6 @@ add_action("rest_api_init", function () {
   ]);
 });
 
-// include_once('updater.php');
-
-// if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-//   $config = array(
-//     'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-//     'proper_folder_name' => 'griptel-oneco-edi', // this is the name of the folder your plugin lives in
-//     'api_url' => 'https://api.github.com/repos/codehouseno/griptel_wc_plugin', // the GitHub API url of your GitHub repo
-//     'raw_url' => 'https://raw.github.com/codehouseno/griptel_wc_plugin/main', // the GitHub raw url of your GitHub repo
-//     'github_url' => 'https://github.com/codehouseno/griptel_wc_plugin', // the GitHub url of your GitHub repo
-//     'zip_url' => 'https://github.com/codehouseno/griptel_wc_plugin/zipball/master', // the zip url of the GitHub repo
-//     'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-//     'requires' => '5.7', // which version of WordPress does your plugin require?
-//     'tested' => '5.7', // which version of WordPress is your plugin tested up to?
-//     'readme' => 'README.md', // which file to use as the readme for the version number
-//     'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
-//   );
-//   new WP_GitHub_Updater($config);
-// }
-
 add_action('rest_api_init', function () {
   register_rest_route('codehouse/v1', '/updatePlugin', array(
       'methods' => 'POST',
